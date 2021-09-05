3c61f54460021182bcce34c1526f65175e78f3ad
kaito-a-bit <arakai0128@gmail.com>
2021-09-05 22:27:52 +0900

    added badics for this app.

---
+++ BookManageApp/QRRecognitionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookManageApp
+//
+//  Created by 荒井海斗 on 2021/09/05.
+//
+
+import UIKit
+import AVFoundation
+
+class QRRecognitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
